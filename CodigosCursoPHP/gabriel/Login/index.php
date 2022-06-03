b370a6c38b3359f86fa206a0d0f400be929c8ce6
Gabriel Mentges <gabrielmentges@gmail.com>
2022-06-03 16:37:30 -0300

    Adicionei a pasta loginESenha com um novo sistema de login e senha criptografada e mudanças nos arquivos da pasta Login

---
+++ CodigosCursoPHP/gabriel/Login/index.php
@@ -0,0 +1,74 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<link rel="stylesheet" type="text/css" href="style.css"/>
+<title>Login e Senha criptografada</title>
+</head>
+<body>
+<form id="login" method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+    <ul>
+        <li>
+            <label for="login">Login</label>
+            <input type="text" id="login" name="login"/>
+            <?php if(isset($erros['login'])) { ?>
+            <label class="erro" for="login"><?php echo $erros['login']; ?></label>
+            <?php } ?>
+        </li>
+        <li>
+            <label for="senha">Senha</label>
+            <input type="password" id="senha" name="senha"/>
+            <?php if(isset($erros['senha'])) { ?>
+            <label class="erro" for="senha"><?php echo $erros['senha']; ?></label>
+            <?php } ?>
+        </li>
+        <li>
+            <input type="submit" id="entrar" value="Acessar"/>
+        </li>
+    </ul>
+</form> <!-- #login -->
+<?php if(isset($erros['acesso_erro'])) { ?>
+<div id="aviso">
+    <p><?php echo $erros['acesso_erro']; ?></p>
+</div><!-- #aviso -->
+<?php } ?>
+</body>
+</html>
+
+<?php
+/* VERIFICO SE OS DADOS DO FORMULÁRIO FORAM ENVIADOS */
+if(count($_POST) > 0) {
+    $erros = array();
+    $dados = array();
+ 
+    foreach($_POST as $campo => $valor) {
+        /* SE O CAMPO VIER VAZIO */
+        if(empty($valor)) {
+            $erros[$campo] = 'O campo <strong>'.ucwords($campo).'</strong> não pode ficar vazio';
+        } else {
+            $dados[$campo] = $valor;
+        }
+    }
+    /* VERIFICO SE NÃO EXISTEM ERROS  */
+    if(count($erros) == 0) {
+        /* FAÇO A CONEXÃO COM O BANCO DE DADOS E COM A BASE DE DADOS acesso */
+        $conn = new mysqli('localhost', 'root', '1234','acesso');
+        if ($conn->connect_error) {
+            die('Falha ao estabelecer uma conexão: '.$conn->connect_error);
+        } else {
+            $login = $dados['login'];
+            $senha = md5($dados['senha']);
+ 
+            $verificar = $conn->query('SELECT * FROM login WHERE login = \''.$login.'\' AND senha = \''.$senha.'\' ');
+ 
+            /* SE O LOGIN E SENHA ESTÃO CORRETOS REDIRECIONO PARA A PÁGINA DE SUCESSO */
+            if($verificar->num_rows > 0) {
+                header('location: sucesso.php');
+            } else {
+                /* CASO NÃO ESTEJA, GRAVO UMA MENSAGEM DE AVISO */
+                $erros['acesso_erro'] = 'Você não tem cadastro ou seus dados estão incorretos';
+            }
+        }
+    }
+}
+?>
---
+++ CodigosCursoPHP/gabriel/loginESenha/sucesso.php
@@ -0,0 +1,11 @@
+﻿<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Sucesso</title>
+</head>
+
+<body>
+<p>Parabéns... Você acessou o sistema!</p>
+</body>
+</html>
