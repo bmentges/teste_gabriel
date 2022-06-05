a736eff4c2e282316270c05bdc0978098ccffefd
Gabriel Mentges <gabrielmentges@gmail.com>
2022-06-05 12:20:19 -0300

    Primeiro commit do arquivo incial do Projeto Entra e Sai

---
+++ CodigosCursoPHP/gabriel/innout-final/innout/src/views/day_records.php
@@ -0,0 +1,43 @@
+<main class="content">
+    <?php
+        renderTitle(
+            'Registrar Ponto',
+            'Mantenha seu ponto consistente!',
+            'icofont-check-alt'
+        );
+        include(TEMPLATE_PATH . "/messages.php");
+    ?>
+    <div class="card">
+        <div class="card-header">
+            <h3><?= $today ?></h3>
+            <p class="mb-0">As marcações efetuadas hoje</p>
+        </div>
+        <div class="card-body">
+            <div class="d-flex m-5 justify-content-around">
+                <span class="record">Entrada: <?= $workingHours->time1 ?? '---' ?></span>
+                <span class="record">Intervalo: <?= $workingHours->time2 ?? '---' ?></span>
+            </div>
+            <div class="d-flex m-5 justify-content-around">
+                <span class="record">Retorno: <?= $workingHours->time3 ?? '---' ?></span>
+                <span class="record">Saída: <?= $workingHours->time4 ?? '---' ?></span>
+            </div>
+        </div>
+        <div class="card-footer d-flex justify-content-center">
+            <a href="innout.php" class="btn btn-success btn-lg">
+                <i class="icofont-check mr-1"></i>
+                Marcar o Ponto
+            </a>
+        </div>
+    </div>
+
+    <form class="mt-5" action="innout.php" method="post">
+        <div class="input-group no-border">
+            <input type="text" name="forcedTime" class="form-control"
+                placeholder="Informe a hora para simular o batimento">
+            <button class="btn btn-danger ml-3">
+                Simular Ponto
+            </button>
+        </div>
+    </form>
+
+</main>
---
+++ CodigosCursoPHP/gabriel/innout-final/innout/src/views/template/footer.php
@@ -0,0 +1,6 @@
+<footer class="footer">
+    <span>Desenvolvido por <span class="text-danger">BrothersDev</span></span>
+</footer>
+<script src="assets/js/app.js"></script>
+</body>
+</html>
